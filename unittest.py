abb95a9f6852c96715ad313db1059f57674c424c
atmosphere2431 <39825464+atmosphere2431@users.noreply.github.com>
2018-06-03 15:16:01 +0900

    Create unittest.py

---
+++ unittest.py
@@ -0,0 +1,12 @@
+import unittest
+from plus import plus
+
+class TestPlus(unittest.TestCase):
+    def test_init(self):
+        self.assertqual(plus(1,2),3,"error onn int t")
+    def test_float(self):
+        self.assertTrue(3.299999 < plus (1.1,2.2) < 3.300001,"error on float")
+    def test_str(self):
+        self.assertEqual(plus"ab","cd"),"abcd","error on str")
+
+unittestmain()
