446853f0686e313878becc98cd58020b3669c48c
atmosphere2431 <39825464+atmosphere2431@users.noreply.github.com>
2018-06-03 16:20:18 +0900

    Update unittest.py

--- unittest.py
+++ unittest.py
@@ -7,6 +7,6 @@ class TestPlus(unittest.TestCase):
     def test_float(self):
         self.assertTrue(3.299999 < plus (1.1,2.2) < 3.300001,"error on float")
     def test_str(self):
-        self.assertEqual(plus"ab","cd"),"abcd","error on str")
+        self.assertEqual(plus("ab","cd"),"abcd","error on str")
 
 unittestmain()
